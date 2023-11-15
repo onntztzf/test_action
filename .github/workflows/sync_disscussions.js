cbb7bc67eb34cabacd5ed19ed4121598a6c54ee4
zhangpeng <zhangpeng.0304@aliyun.com>
2023-11-16 02:46:01 +0800

    update

--- .github/workflows/sync_disscussions.js
+++ .github/workflows/sync_disscussions.js
@@ -193,7 +193,7 @@ async function main() {
     writePromises.push(writeToFileSync(mdFilePath, `---\n${frontMatter}\n---\n\n${markdownTitle}\n\n${markdownBody}\n`));
 
     const labels = v.labels?.nodes.map(label => `[${label.name}](https://github.com/onntztzf/test_action/discussions?discussions_q=label%3A${label.name})`);
-    READMEData.push([`[${metadata.category}](https://github.com/onntztzf/test_action/discussions/categories/${v.category?.slug}?discussions_q=)`, `[${v.title}](${year}/${month}/${v.number}_${v.id}.md)`, labels.join(", "), metadata.updatedAt]);
+    READMEData.push([`[${metadata.category}](https://github.com/onntztzf/test_action/discussions/categories/${v.category?.slug}?discussions_q=)`, `[${v.title}](${year}/${month}/${v.number}_${v.id}.md)`, labels.join(", ") || "-", metadata.updatedAt]);
 
     const key = `${year}/${month}`;
     SUMMARYData.set(key, [...(SUMMARYData.get(key) || []), `[${v.title}](${year}/${month}/${v.number}_${v.id}.md)`]);
