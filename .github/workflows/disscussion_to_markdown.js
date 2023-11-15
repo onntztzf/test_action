f6a38bb0c6e6e91452a0a41ea7a71ea31fbc837d
zhangpeng <zhangpeng.0304@aliyun.com>
2023-11-15 17:14:50 +0800

    update

--- .github/workflows/disscussion_to_markdown.js
+++ .github/workflows/disscussion_to_markdown.js
@@ -1,27 +1,31 @@
 const { graphql } = require("@octokit/graphql");
+const dayjs = require('dayjs')
+const utc = require('dayjs/plugin/utc');
+dayjs.extend(utc);
+
 const fs = require('fs').promises;
 const path = require('path');
 
 // Asynchronous function to write data to a file
 async function writeToFileSync(filePath, data) {
-    try {
-        const directory = path.dirname(filePath);
-        await fs.mkdir(directory, { recursive: true });
-        await fs.writeFile(filePath, data);
-        console.log('File written successfully:', filePath);
-    } catch (error) {
-        console.error('Error writing file:', error);
-        throw error; // Stop handling errors
-    }
+  try {
+    const directory = path.dirname(filePath);
+    await fs.mkdir(directory, { recursive: true });
+    await fs.writeFile(filePath, data);
+    console.log('File written successfully:', filePath);
+  } catch (error) {
+    console.error('Error writing file:', error);
+    throw error; // Stop handling errors
+  }
 }
 
 // Asynchronous function to fetch discussions from a GitHub repository
 async function fetchDiscussions(token, owner, repo, limit = 10) {
-    let hasMore = true;
-    let afterCursor = null;
-    const all_discussions = [];
+  let hasMore = true;
+  let afterCursor = null;
+  const all_discussions = [];
 
-    const query = `
+  const query = `
     query get_discussions($owner: String!, $repo: String!, $after: String, $limit: Int = 10) {
         repository(owner:$owner, name: $repo) {
             discussions(first: $limit, after:$after) {
@@ -68,128 +72,187 @@ async function fetchDiscussions(token, owner, repo, limit = 10) {
           }
     }`;
 
-    // Create a GraphQL client with authentication using the provided access token
-    const graphqlWithAuth = graphql.defaults({
-        headers: {
-            authorization: `token ${token}`,
-        },
-    });
+  // Create a GraphQL client with authentication using the provided access token
+  const graphqlWithAuth = graphql.defaults({
+    headers: {
+      authorization: `token ${token}`,
+    },
+  });
 
-    while (hasMore) {
-        try {
-            const response = await graphqlWithAuth(query, { owner, repo, after: afterCursor, limit });
-            const discussions = response.repository.discussions.nodes;
-            all_discussions.push(...discussions);
+  while (hasMore) {
+    try {
+      const response = await graphqlWithAuth(query, { owner, repo, after: afterCursor, limit });
+      const discussions = response.repository.discussions.nodes;
+      all_discussions.push(...discussions);
 
-            const pageInfo = response.repository.discussions.pageInfo;
-            hasMore = pageInfo.hasNextPage;
-            afterCursor = pageInfo.endCursor;
-        } catch (error) {
-            console.error('Error fetching discussions:', error.message);
-            throw error;
-        }
+      const pageInfo = response.repository.discussions.pageInfo;
+      hasMore = pageInfo.hasNextPage;
+      afterCursor = pageInfo.endCursor;
+    } catch (error) {
+      console.error('Error fetching discussions:', error.message);
+      throw error;
     }
+  }
 
-    return all_discussions;
+  return all_discussions;
 }
 
 // Main asynchronous function to orchestrate the process
 async function main() {
 
-    console.log(process.env)
+  console.log(process.env)
 
-    const repo = process.env.GITHUB_REPOSITORY;
+  const repo = process.env.GITHUB_REPOSITORY;
 
-    // 使用 split 方法将字符串拆分为数组
-    const parts = repo.split("/");
+  // 使用 split 方法将字符串拆分为数组
+  const parts = repo.split("/");
 
-    // parts[0] 包含用户名，parts[1] 包含仓库名称
-    const username = parts[0];
-    const repoName = parts[1];
+  // parts[0] 包含用户名，parts[1] 包含仓库名称
+  const username = parts[0];
+  const repoName = parts[1];
 
-    console.log("Username:", username);
-    console.log("Repository Name:", repoName);
+  console.log("Username:", username);
+  console.log("Repository Name:", repoName);
 
-    console.log('Fetching discussions...');
-    let allDiscussions = await fetchDiscussions(process.env.GITHUB_TOKEN, username, repoName);
-    console.log('Fetched', allDiscussions.length, 'discussions.');
+  console.log('Fetching discussions...');
+  let allDiscussions = await fetchDiscussions(process.env.GITHUB_TOKEN, username, repoName);
+  console.log('Fetched', allDiscussions.length, 'discussions.');
 
-    // Filter discussions where the author association is the OWNER
-    allDiscussions = allDiscussions.filter(discussion => discussion.authorAssociation === "OWNER");
+  const discussionMap = new Map();
+  for (let i = 0; i < allDiscussions.length; i++) {
+    const v = allDiscussions[i];
+    if (v.authorAssociation !== "OWNER") {
+      continue
+    }
+    const key = `${v.number}_${v.id}`;
+    const existing = discussionMap.get(key);
+    if (!existing || dayjs(v.updatedAt).isAfter(dayjs(existing.updatedAt))) {
+      discussionMap.set(key, v);
+    }
+  }
+  const finalDiscussions = discussionMap.values()
 
-    allDiscussions.sort((a, b) => a.updatedAt.localeCompare(b.updatedAt));
+  console.log('Final', finalDiscussions.length, 'discussions.');
 
-    const contents = [];
-    const writePromises = allDiscussions.map(async (discussion) => {
-        try {
-            const jsonFilePath = `discussions/${discussion.number}_${discussion.id}.json`;
-            await writeToFileSync(jsonFilePath, JSON.stringify(discussion, null, 2));
 
-            const updatedAtInUTC = new Date(discussion.updatedAt);
-            const updatedAt = updatedAtInUTC.toLocaleString("zh-Hans", { timeZone: "Asia/Shanghai" });
+  // const discussionMap = new Map();
+  // for (let i = 0; i < allDiscussions.length; i++) {
+  //   const v = allDiscussions[i];
+  //   if (v.authorAssociation !== "OWNER") {
+  //     continue
+  //   }
+  //   const updatedAtInUTC = dayjs(v.updatedAt);
 
-            const metadata = {
-                author: discussion.author?.login || '',
-                category: (discussion.category?.emojiHTML ? discussion.category.emojiHTML.match(/>(.*?)</)?.[1] + ' ' : '') + (discussion.category?.name || ''),
-                labels: (discussion.labels?.nodes || []).map(label => label.name).join(', '),
-                discussion: discussion.url || '',
-                updated_at: updatedAt || '',
-            };
+  //   const key = `${v.number}_${v.id}`;
+  //   const existing = discussionMap.get(key);
+  //   if (!existing || existing.updateTime > existing.updateTime) {
+  //     discussionMap.set(key, obj);
+  //   }
 
-            const frontMatter = Object.entries(metadata)
-                .map(([key, value]) => `${key}: ${value}`)
-                .join('\n');
 
-            const markdownTitle = `# ${discussion.title || 'Unknown Title'}`;
-            const markdownBody = discussion.body.trim() || 'No content';
 
-            const createdAtInUTC = new Date(discussion.createdAt);
-            const createdAtInCST = new Date(createdAtInUTC.toLocaleString("zh-Hans", { timeZone: "Asia/Shanghai" }));
-            // 获取年份
-            const year = createdAtInCST.getFullYear();
-            // 获取月份（注意：月份从 0 开始，所以需要加 1）
-            const month = createdAtInCST.getMonth() + 1;
+  //   const jsonFilePath = `discussions/${v.number}_${v.id}.json`;
+  //   const createdAtInUTC = dayjs(v.createdAt);
 
-            const mdFilePath = `markdowns/${year}/${month}/${discussion.number}_${discussion.id}.md`;
-            contents.push([`${year}/${month}`, `[${discussion.title}](${year}/${month}/${discussion.number}_${discussion.id}.md)`, updatedAt])
-            await writeToFileSync(mdFilePath, `---\n${frontMatter}\n---\n\n${markdownTitle}\n\n${markdownBody}\n`);
-        } catch (error) {
-            console.error('Error processing discussion:', error);
-            throw error;
-        }
-    });
+  //   const metadata = {
+  //     author: v.author?.login || '',
+  //     category: `${v.category?.emojiHTML ? v.category.emojiHTML.match(/>(.*?)</)?.[1] + ' ' : ''}${v.category?.name || ''}`,
+  //     labels: (v.labels?.nodes || []).map(label => label.name).join(', '),
+  //     discussion: v.url || '',
+  //     updated_at: updatedAtInUTC.utcOffset(8).format('YYYY-MM-DD HH:mm:ss') || '',
+  //   };
+  //   const frontMatter = Object.entries(metadata)
+  //     .map(([key, value]) => `${key}: ${value}`)
+  //     .join('\n');
+  //   const markdownTitle = `# ${discussion.title || 'Unknown Title'}`;
+  //   const markdownBody = discussion.body.trim() || 'No content';
+  //   const createdAtInCST = createdAtInUTC.utcOffset(8)
+  //   // 获取年份
+  //   const year = createdAtInCST.year();
+  //   // 获取月份（注意：月份从 0 开始，所以需要加 1）
+  //   const month = createdAtInCST.month() + 1;
+  //   const mdFilePath = `markdowns/${year}/${month}/${discussion.number}_${discussion.id}.md`;
+  //   return {}
+  // }
 
-    await Promise.all(writePromises);
+  // const writePromises = allDiscussions.map(async (discussion) => {
+  //   try {
+  //     const jsonFilePath = `discussions/${discussion.number}_${discussion.id}.json`;
+  //     await writeToFileSync(jsonFilePath, JSON.stringify(discussion, null, 2));
+  //   } catch (error) {
+  //     console.error('Error processing discussion:', error);
+  //     throw error;
+  //   }
+  // });
 
-    // contents.reverse()
+  // const contents = [];
+  // const writePromises = allDiscussions.map(async (discussion) => {
+  //   try {
+  //     const jsonFilePath = `discussions/${discussion.number}_${discussion.id}.json`;
+  //     await writeToFileSync(jsonFilePath, JSON.stringify(discussion, null, 2));
 
-    // let README = "# README\n\n";
-    // README += "Just a repository for blogs. :)\n\n";
-    // README += "## Table of Contents\n\n";
-    // README += "| Directory | File | Last Updated |\n";
-    // README += "| --- | --- | --- |\n";
-    // for (let i = 0; i < contents.length; i++) {
-    //     const v = contents[i];
-    //     README += `| ${v[0]} | ${v[1]} | ${v[2]} |\n`;
-    // }
-    // README += "\n如果觉得文章不错，可以关注公众号哟！\n\n"
-    // README += "![干货输出机](https://file.zhangpeng.site/wechat/qrcode.jpg)"
-    // await writeToFileSync("README.md", README)
+  // const updatedAtInUTC = new Date(discussion.updatedAt);
+  // const updatedAt = updatedAtInUTC.toLocaleString("zh-Hans", { timeZone: "Asia/Shanghai" });
 
-    // let SUMMARY = "# SUMMARY\n\n";
-    // SUMMARY += "Just a repository for blogs. :)\n\n";
-    // SUMMARY += "## Table of Contents\n\n";
-    // SUMMARY += "| Directory | File | Last Updated |\n";
-    // SUMMARY += "| --- | --- | --- |\n";
-    // for (let i = 0; i < contents.length; i++) {
-    //     const v = contents[i];
-    //     README += `| ${v[0]} | ${v[1]} | ${v[2]} |\n`;
-    // }
-    // SUMMARY += "\n如果觉得文章不错，可以关注公众号哟！\n\n"
-    // SUMMARY += "![干货输出机](https://file.zhangpeng.site/wechat/qrcode.jpg)"
-    // await writeToFileSync("README.md", SUMMARY)
+  // const metadata = {
+  //   author: discussion.author?.login || '',
+  //   category: (discussion.category?.emojiHTML ? discussion.category.emojiHTML.match(/>(.*?)</)?.[1] + ' ' : '') + (discussion.category?.name || ''),
+  //   labels: (discussion.labels?.nodes || []).map(label => label.name).join(', '),
+  //   discussion: discussion.url || '',
+  //   updated_at: updatedAt || '',
+  // };
 
-    console.log("Done. Total discussions:", allDiscussions.length);
+  //     const frontMatter = Object.entries(metadata)
+  //       .map(([key, value]) => `${key}: ${value}`)
+  //       .join('\n');
+
+  //     const markdownTitle = `# ${discussion.title || 'Unknown Title'}`;
+  //     const markdownBody = discussion.body.trim() || 'No content';
+
+  //     const createdAtInUTC = new Date(discussion.createdAt);
+  //     const createdAtInCST = new Date(createdAtInUTC.toLocaleString("zh-Hans", { timeZone: "Asia/Shanghai" }));
+  //     // 获取年份
+  //     const year = createdAtInCST.getFullYear();
+  //     // 获取月份（注意：月份从 0 开始，所以需要加 1）
+  //     const month = createdAtInCST.getMonth() + 1;
+
+  //     const mdFilePath = `markdowns/${year}/${month}/${discussion.number}_${discussion.id}.md`;
+  //     contents.push([`${year}/${month}`, `[${discussion.title}](${year}/${month}/${discussion.number}_${discussion.id}.md)`, updatedAt])
+  //     await writeToFileSync(mdFilePath, `---\n${frontMatter}\n---\n\n${markdownTitle}\n\n${markdownBody}\n`);
+  //   } catch (error) {
+  //     console.error('Error processing discussion:', error);
+  //     throw error;
+  //   }
+  // });
+  // await Promise.all(writePromises);
+
+  // let README = "# README\n\n";
+  // README += "Just a repository for blogs. :)\n\n";
+  // README += "## Table of Contents\n\n";
+  // README += "| Directory | File | Last Updated |\n";
+  // README += "| --- | --- | --- |\n";
+  // for (let i = 0; i < contents.length; i++) {
+  //     const v = contents[i];
+  //     README += `| ${v[0]} | ${v[1]} | ${v[2]} |\n`;
+  // }
+  // README += "\n如果觉得文章不错，可以关注公众号哟！\n\n"
+  // README += "![干货输出机](https://file.zhangpeng.site/wechat/qrcode.jpg)"
+  // await writeToFileSync("README.md", README)
+
+  // let SUMMARY = "# SUMMARY\n\n";
+  // SUMMARY += "Just a repository for blogs. :)\n\n";
+  // SUMMARY += "## Table of Contents\n\n";
+  // SUMMARY += "| Directory | File | Last Updated |\n";
+  // SUMMARY += "| --- | --- | --- |\n";
+  // for (let i = 0; i < contents.length; i++) {
+  //     const v = contents[i];
+  //     README += `| ${v[0]} | ${v[1]} | ${v[2]} |\n`;
+  // }
+  // SUMMARY += "\n如果觉得文章不错，可以关注公众号哟！\n\n"
+  // SUMMARY += "![干货输出机](https://file.zhangpeng.site/wechat/qrcode.jpg)"
+  // await writeToFileSync("README.md", SUMMARY)
+
+  console.log("Done. Total discussions:", allDiscussions.length);
 }
 
 // Execute the main function to start the process
